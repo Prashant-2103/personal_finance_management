a58983836d2a67602b9a7520a1a368bb34c1d4c3
Prashant-2103 <prashmessi6@gmail.com>
2026-01-15 19:11:35 +0530

    Added config.js file for google firebase configs

--- firebase.js
+++ firebase.js
@@ -10,16 +10,7 @@ import {
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCPBHY4-PE5i-HyIBQNI6d4HsZVISF9PxM",
-    authDomain: "personal-finance-managem-f3153.firebaseapp.com",
-    projectId: "personal-finance-managem-f3153",
-    storageBucket: "personal-finance-managem-f3153.firebasestorage.app",
-    messagingSenderId: "615466262785",
-    appId: "1:615466262785:web:554ae49aa779d394c5bb00",
-    measurementId: "G-W21MNXMBE0"
-  };
-
+ 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   // const analytics = getAnalytics(app);
